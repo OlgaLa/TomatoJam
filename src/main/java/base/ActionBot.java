7f6af7d98a913c85b8b7952e10f202c84c1aaa82
Olga Lapanovich <o.wartooth@gmail.com>
2017-12-21 00:42:26 +0300

    Bloody drag and drop

--- src/main/java/base/ActionBot.java
+++ src/main/java/base/ActionBot.java
@@ -1,15 +1,15 @@
 package base;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.*;
 import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.awt.*;
+
+import static com.sun.xml.internal.ws.client.ContentNegotiation.none;
 
 /**
  * Created by Olga Lapanovich on 18.12.2017.
@@ -42,59 +42,16 @@ public class ActionBot {
         wait.until(ExpectedConditions.presenceOfElementLocated(locator));
     }
 
-    public void waitUntilClickable(By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(locator));
-    }
-
-    public void dragAndDrop(WebElement locatorFrom, By locatorTo) {
+    public void dragAndDrop(WebElement from, By locatorTo) {
+//        So, here is the deal:
+//        It seems to be a known bug with java selenium webdriver https://bugs.chromium.org/p/chromedriver/issues/detail?id=841.
+//        It's getting fixed over and over again since 2014, oh.
+//        I tried all the possible combinations of known workarounds but didn't manage to make it work.
+//        Anyway, I think it doesn't make any sense to keep trying anymore.
         WebElement to = driver.findElement(locatorTo);
         Actions builder = new Actions(driver);
-        Action dragAndDrop = builder.dragAndDrop(locatorFrom, to).build();
+        Action dragAndDrop = builder.dragAndDrop(from, to).build();
         dragAndDrop.perform();
-//        Action dragAndDrop = builder.clickAndHold(locatorFrom)
-//                .moveToElement(to)
-//                .release(locatorFrom)
-//                .build();
-//        dragAndDrop.perform();
-//        WebElement to = driver.findElement(locatorTo);
-//        final Actions action = new Actions(driver);
-//       // action.dragAndDrop(locatorFrom, to).build().perform();
-//        int srcX = locatorFrom.getLocation().getX();
-//        int srcY = locatorFrom.getLocation().getY();
-//        int targetX = to.getLocation().getX() + 40;
-//        int targetY = to.getLocation().getY() + 10;
-//
-//        int offsetX = targetX - srcX;
-//        int offsetY = targetY - srcY;
-//        int xOffset = to.getLocation().getX() + to.getSize().getWidth() / 2;
-//        int yOffset = to.getLocation().getY() + to.getSize().getHeight() / 2;
-//        action.dragAndDropBy(locatorFrom, offsetX,offsetY).build().perform();
-//        action.clickAndHold(locatorFrom)
-//                .moveByOffset(offsetX,offsetY)
-//                .pause(5000)
-//                .release()
-//                .build()
-//                .perform();
-//        Point coordinates1 = locatorFrom.getLocation();
-//        Point coordinates2 = to.getLocation();
-//
-//        Robot robot = null;
-//        try {
-//            robot = new Robot();
-//        } catch (AWTException e) {
-//            e.printStackTrace();
-//        }
-//
-//        robot.mouseMove(coordinates1.getX(), coordinates1.getY());
-//        try {
-//            Thread.sleep(1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//        action.dragAndDrop(locatorFrom, to).perform();
-//        action.moveByOffset(offsetX, offsetY).perform();
-//        action.release().perform();
-//        action.clickAndHold(locatorFrom).release(to).perform();
     }
 
 }
